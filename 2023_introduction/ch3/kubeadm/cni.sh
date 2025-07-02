ae2968ce730500e90fd9b141026eab04446dbb10
baconyao <bacon735392@gmail.com>
2025-07-02 14:07:33 -0400

    Keep only network related materials

--- 2023_introduction/ch3/kubeadm/cni.sh
+++
@@ -1,2 +0,0 @@
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml
